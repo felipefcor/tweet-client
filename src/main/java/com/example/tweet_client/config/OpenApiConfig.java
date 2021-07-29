38daccb8d9550d5a8fba5d5b97d071045e46a36f
FelipeFernandez <f.fernandez@kubikdata.com>
2021-07-29 18:01:37 +0200

    feat: add open api configuration and dependencies

---
+++ src/main/java/com/example/tweet_client/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.example.tweet_client.config;
+
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Tweet client API")
+                        .description("Tweet consuming and persistence API")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Tweet client API")
+                        .url("https://github.com/felipefcor/tweet-client"));
+    }
+}
