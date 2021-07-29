9271e5d84d452b8b68ef39c50f770ed8819d3201
FelipeFernandez <f.fernandez@kubikdata.com>
2021-07-29 17:43:13 +0200

    initial setup

---
+++ src/main/java/com/example/tweet_client/TweetClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.tweet_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetClientApplication.class, args);
+	}
+
+}
