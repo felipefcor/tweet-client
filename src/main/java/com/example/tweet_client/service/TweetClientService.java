137ea9358ce111f9fed871d739d222b57f7ecfff
FelipeFernandez <f.fernandez@kubikdata.com>
2021-07-30 11:48:59 +0200

    feat: add filter for languages and condition for location

--- src/main/java/com/example/tweet_client/service/TweetClientService.java
+++ src/main/java/com/example/tweet_client/service/TweetClientService.java
@@ -5,13 +5,12 @@ import com.example.tweet_client.repository.TweetClientRepository;
 import com.example.tweet_client.twitter.TwitterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import twitter4j.Place;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class TweetClientService implements TweetClientServiceInterface{
@@ -27,10 +26,16 @@ public class TweetClientService implements TweetClientServiceInterface{
 
         List<Status> timeline = twitterService.getTimeLine();
         return timeline.stream()
-           .filter(status -> status.getUser().getFollowersCount() > 1500)
-            .filter(status -> status.getLang().equals("en"))
-            .map(status -> new Tweet(status.getId(), status.getUser().getName(),
-                status.getText(), null, false))
-            .collect(Collectors.toList());
+                .filter(status -> status.getUser().getFollowersCount() > 1500)
+                .filter(status -> status.getLang().equals("en") || status.getLang().equals("es") ||
+                                  status.getLang().equals("it") || status.getLang().equals("fr"))
+                .map(status -> new Tweet(status.getId(), status.getUser().getName(),
+                     status.getText(), existLocation(status.getPlace()) ?
+                        status.getPlace().getFullName() : null, false))
+                .collect(Collectors.toList());
+    }
+
+    private boolean existLocation(Place place) {
+        return place != null;
     }
 }
